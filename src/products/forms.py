d1176bf30279f947c56cfd4788f8d0be8fd90869
Vitautas Sabenin <vitautassabenin97@gmail.com>
2020-02-02 01:41:02 +0300

    lern

---
+++ src/products/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', 
+                widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
+    description = forms.CharField(required=False, 
+                                  widget=forms.Textarea(
+                                      attrs={
+                                        'placeholder': 'Your description',
+                                        'class': 'new-class-name two',
+                                        'id': 'my-id-for-textarea',
+                                        'rows': 20,
+                                        'cols': 120
+                                    }
+                                )
+                            )
+    price = forms.DecimalField(initial=199.99)
+    summary = forms.CharField(widget=forms.Textarea(
+                                    attrs={
+                                        'placeholder': 'Your summary',
+                                        'class': 'new-class-name two',
+                                        'id': 'my-id-for-textarea',
+                                        'rows': 20,
+                                        'cols': 120
+                                    }
+                                )
+                            )
+    class Meta:
+        model = Product
+        fields = [
+             'title',
+             'description',
+             'price',
+             'summary'
+        ]
+    
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
+    description = forms.CharField(required=False, 
+                                  widget=forms.Textarea(
+                                      attrs={
+                                        'placeholder': 'Your description',
+                                        'class': 'new-class-name two',
+                                        'id': 'my-id-for-textarea',
+                                        'rows': 20,
+                                        'cols': 120
+                                      }
+                                  )
+                              )
+    price = forms.DecimalField(initial=199.99)
